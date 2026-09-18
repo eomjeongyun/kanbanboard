5071db402d3884cbc1f64233593a41277578d5a5
eomjeongyun <eomjeongyun@users.noreply.github.com>
2026-09-18 11:06:05 +0900

    apply 영덕바다체 font

--- fonts/PLACEHOLDER-README.txt
+++
@@ -1,1 +0,0 @@
-교보 손글씨 2025 이유빈체 파일을 구하면 fonts/YubinHandwriting.woff2 로 넣고 style.css의 @font-face 주석을 해제하세요
--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = 'kanbanboard-shell-v1';
+const CACHE = 'kanbanboard-shell-v2';
 const SHELL = [
   './', './index.html', './style.css', './app.js', './manifest.webmanifest',
+  './fonts/YeongdeokSea.woff2',
   './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'
 ];
 
